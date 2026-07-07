f1c7d95443aa498a612aa04a8078095170ea44b7
Ade Juniawan <42667988+adejuniawan@users.noreply.github.com>
2026-07-07 10:48:53 +0700

    Update and rename index.jsx to src/index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ProjectDashboard from './ProjectDashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ProjectDashboard />
+  </React.StrictMode>
+);
