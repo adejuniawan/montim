2cd7c7a6ce471cf222e8de709763d7a289093359
Ade Juniawan <42667988+adejuniawan@users.noreply.github.com>
2026-07-07 11:36:09 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // <--- PASTIKAN BARIS INI ADA
 import ProjectDashboard from './ProjectDashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
